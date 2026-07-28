7cd61ea4fe04a7f6842314bfdbc3c177af58750c
DEV-BR0 <lazizbekxoljigitov@gmail.com>
2026-07-28 21:10:12 +0500

    sanr

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,17 @@
+import { Wallet } from 'lucide-react'
+
 export const metadata = {
   title: 'Fiscal Atelier | Login',
 }
 
 export default function LoginPage() {
-  return <h1>Login</h1>
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex  flex-col justify-center items-center w-[400px]">
+        <div className="w-[30%]">
+          <Wallet className='w-full text-white bg-[] '/>
+        </div>
+      </div>
+    </div>
+  )
 }
